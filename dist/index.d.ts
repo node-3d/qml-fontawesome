45611e2bf4af7f55cfdfad667cb621153ccf5d92
Luis Blanco <luisblanco1337@gmail.com>
2026-07-07 00:05:08 +0400

    Stop tracking generated build artifacts

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const absPath: string;
-declare const qmlFontawesome: {
-    absPath: string;
-};
-export default qmlFontawesome;
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-const absPath = import.meta.dirname;
-const qmlFontawesome = {
-    absPath: absPath
-};
-const ts = qmlFontawesome;
-export default ts;
-export { absPath };
